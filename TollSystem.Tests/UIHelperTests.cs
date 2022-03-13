726a82e4f960d0364ef7759bf01f27ca58242ca9
Jaro Polhos <56872207+b8006434@users.noreply.github.com>
2022-03-13 19:08:31 +0000

    Added unit test

--- TollSystem.Tests/UIHelperTests.cs
+++ TollSystem.Tests/UIHelperTests.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TollSystemServices;
 
 namespace TollSystem.Tests
 {
     class UIHelperTests
     {
+        [Test]
+        public void ChangeColorBrightness_ReturnValidResult()
+        {
+            var result = UIHelper.ChangeColorBrightness(Color.Red, 20);
+
+            Assert.That(result.A == 255 && result.B == 236 && 
+                        result.G == 236 && result.R == 255);
+        }
     }
 }
